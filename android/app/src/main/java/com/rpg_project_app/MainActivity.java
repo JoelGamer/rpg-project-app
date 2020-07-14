9a5cf4ad2520567235494866a0aaca5089e1e3b1
JoelGamer <gui.theodoro2010@gmail.com>
2020-07-14 17:16:23 -0300

    RPG-APP: Initial Setup

---
+++ android/app/src/main/java/com/rpg_project_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rpg_project_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rpg_project_app";
+  }
+}
